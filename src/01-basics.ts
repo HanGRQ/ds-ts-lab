f953b84464b403386aab9942b535fa034e2272af
Sihan Ma <masihan0303@gmail.com>
2025-01-24 00:01:53 +0000

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,53 +1,55 @@
 import { Friend, Colleague, ColleagueHistory } from './myTypes';
 
-const friend1: Friend = {
+const friend1 : Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
-  };
+    dob: new Date("1998-11-20") ,
+};
   
-  const friend2: Friend = {
+const friend2 : Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
-  };
-  
-  export const friends: Friend[] = [friend1, friend2];
-  // console.log(friends[1]);
+    interests: ['Music', 'Sport']
+};
 
 
-  const colleague1: Colleague = {
+export const friends: Friend[] = [friend1, friend2];
+// console.log(friends[1]);
+
+
+const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
-      email: "rgraham@company.com",
-      extension: 121,
+        email: "rgraham@company.com",
+        extension: 121,
     },
-  };
-  
-  const colleague2: Colleague = {
+};
+
+const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
-      email: "pburke@company.com",
-      extension: 132,
+        email: "pburke@company.com",
+        extension: 132,
     },
-  };
-  
-  const colleague3: Colleague = {
+};
+
+const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
-      email: "dos@company.com",
-      extension: 125,
+        email: "dos@company.com",
+        extension: 125,
     },
-  };
+};
 
-  // use ColleagueHistory type defines colleague history data
-  export const colleagues: ColleagueHistory = {
+// use ColleagueHistory type defines colleague history data
+export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
-  };
-  
-  // console.log(colleagues.current[0]);
-  
+};
+
+// console.log(colleagues.current[0]);
